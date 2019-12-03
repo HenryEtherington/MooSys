bda0b7689504a04dd9a6eee9a8e475117dd7ba0c
Harry Etherington <43179139+HenryEtherington@users.noreply.github.com>
2019-12-03 17:46:49 +0000

    Update locations.js

--- app_api/models/locations.js
+++ app_api/models/locations.js
@@ -1,17 +1,5 @@
 const mongoose = require('mongoose');
 
-const openingTimeSchema = new mongoose.Schema({
-  days: {
-    type: String,
-    required: true
-  },
-  opening: String,
-  closing: String,
-  closed: {
-    type: Boolean,
-    required: true
-  }
-});
 
 const reviewSchema = new mongoose.Schema({
   author: {
